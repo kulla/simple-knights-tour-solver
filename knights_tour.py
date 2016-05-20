10f22105f6c0190b2a11e72a258882cdbb6a19f4
Stephan Kulla <git.mail@kulla.me>
2016-05-20 16:26:19 +0200

    add code for reading user input

--- knights_tour.py
+++ knights_tour.py
@@ -1,14 +1,39 @@
+import re
+
 class Chessboard:
 
     def __init__(self, n, m):
         "Defines a n x m chess board."
+
+        assert n > 0
+        assert m > 0
+
         self.n = n
         self.m = m
+
+    def find_knights_tour(self, start_x, start_y):
+        pass
 
 class Path:
 
     def __init__(self, chessboard, tail, x, y):
+        assert 0 <= x and x <= chessboard.n
+        assert 0 <= y and y <= chessboard.m
+
         self.chessboard = chessboard # the chessboard
         self.tail = tail # previuos position of the knight
         self.x = x # current x position of the knight
         self.y = y # current y position of the knight
+
+if __name__ == "__main__":
+    print("== What is the size of the chess board? ==")
+    print("Enter the size in the format nxm like '8x8' or '42x23'")
+    print("")
+
+    user_input = input("Size of chess board: ")
+
+    n,m = map(int, re.match(r"(\d+)x(\d+)", user_input).groups())
+
+    chessboard = Chessboard(n,m)
+
+    chessboard.find_knights_tour(0,0)
