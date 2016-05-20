add8fea7941bb118d57244de90cc52e6e6b0d087
Stephan Kulla <git.mail@kulla.me>
2016-05-20 16:12:58 +0200

    add class "Path" with constructor

--- knights_tour.py
+++ knights_tour.py
@@ -4,3 +4,11 @@ class Chessboard:
         "Defines a n x m chess board."
         self.n = n
         self.m = m
+
+class Path:
+
+    def __init__(self, chessboard, tail, x, y):
+        self.chessboard = chessboard # the chessboard
+        self.tail = tail # previuos position of the knight
+        self.x = x # current x position of the knight
+        self.y = y # current y position of the knight
